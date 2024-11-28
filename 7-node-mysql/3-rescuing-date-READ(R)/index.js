cdcd341a60e711798df3eae9d2a074fe4c8e45a0
lorrayne.santana <lorrayne.santana2022@gmail.com>
2024-11-28 17:56:49 -0300

    adicionando porta

--- 7-node-mysql/3-rescuing-date-READ(R)/index.js
+++ 7-node-mysql/3-rescuing-date-READ(R)/index.js
@@ -57,8 +57,9 @@ app.get('/books', (req, res) => {
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Mc@@2024**',
+  password: '',
   database: 'nodemysql',
+  port: '3305'
 })
 
 conn.connect(function (err) {
